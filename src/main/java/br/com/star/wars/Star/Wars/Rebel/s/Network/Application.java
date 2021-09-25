828497150ad09374237e31ec2c0a3c0dc51ea11e
Willian Moya <willian.fmoya@gmail.com>
2021-09-25 15:25:52 -0300

    Initial Commit

---
+++ src/main/java/br/com/star/wars/Star/Wars/Rebel/s/Network/Application.java
@@ -0,0 +1,13 @@
+package br.com.star.wars.Star.Wars.Rebel.s.Network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
